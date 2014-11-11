102ae4c8edfa768e3453d2699059b1ec54aa0da0
Michael Fleming <michael.fleming121@gmail.com>
2014-11-10 22:58:13 -0600

    Added More functionality
    Added Temp, Pressure, Wifi on/off and strength/link speed. Added a check
    for availability of sensors being tested. Added temporary functionality
    to Post Button (button2) to put out a list of all sensors on the device
    for testing purposes. Updates to both SysStrings and the Test activity

--- SCHAS/SysLib/src/main/java/mymodule/app2/mymodule/app2/SysStrings.java
+++ SCHAS/SysLib/src/main/java/mymodule/app2/mymodule/app2/SysStrings.java
@@ -7,7 +7,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.location.Address;
 import android.view.Surface;
@@ -24,6 +28,8 @@ public class SysStrings{
     private static float y;
     private static float z;
     private static float light;
+    private static float pressure;
+    private static float temp;
 
 
 
@@ -86,6 +92,30 @@ public class SysStrings{
                 return "reverse landscape";
         }
     }
+
+    //        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    //        wifiman = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    // Include the above lines in your onCreate method and pass in (connManager,wifiman)
+
+    public static String getWIFI(ConnectivityManager cm,WifiManager wifiManager){
+
+        NetworkInfo mWifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected()) {
+            int linkSpeed = wifiManager.getConnectionInfo().getLinkSpeed();
+            return "wifi is connected at the speed of: " + linkSpeed +"Mbps" + "\nAt a strength of: " + wifiManager.getConnectionInfo().getRssi() + "dBm";
+        }
+        else{return "wifi is not connected";}
+    }
+
+
+    //
+    //
+    public static String getIEMI(TelephonyManager tm){
+        return tm.getDeviceId();
+    }
+
+
     public static void updateGravity(SensorEvent event){
         x = event.values[0];
         y = event.values[1];
@@ -93,15 +123,47 @@ public class SysStrings{
 
 
     }
-    public static String getGravity(){
-        return "x: " + x  + "\ny: " + y  + "\nz: " + z ;
+    public static String getGravity(SensorManager sm){
+        if(sm.getDefaultSensor(Sensor.TYPE_GRAVITY) != null){
+            return "x: " + x + "\ny: " + y + "\nz: " + z;
+        }
+        else{return "Phone has no gravity sensor";}
     }
 
     public static void updateLight(SensorEvent event){
         light = event.values[0];
     }
-    public static String getLight(){
-        return "light = " + light;
+    public static String getLight(SensorManager sm){
+        if (sm.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+            return "light(lux) = " + light;
+
+        }
+        else{return ""+light;}
+
+
     }
+
+    public static void updatePressure(SensorEvent event){
+        pressure = event.values[0];
+    }
+    public static String getPressure(SensorManager sm){
+        if(sm.getDefaultSensor(Sensor.TYPE_PRESSURE) != null) {
+            return "pressure(hPa) = " + pressure;
+        }
+        else{return "Phone has no pressure sensor";}
+
+    }
+
+    public static void updateTemp(SensorEvent event){
+        temp = event.values[0];
+    }
+    public static String getTemp(SensorManager sm){
+        if(sm.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null) {
+            return "Temp(C) = " + temp;
+        }
+        else{return "Phone has no temperature sensor";}
+
+    }
+
 
 }
--- SCHAS/app/src/main/java/org/geospaces/schas/Test.java
+++ SCHAS/app/src/main/java/org/geospaces/schas/Test.java
@@ -8,13 +8,18 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 import mymodule.app2.mymodule.app2.SysStrings;
 
@@ -28,10 +33,13 @@ public class Test extends Activity implements SensorEventListener {
     private Sensor mAccelerometer;
     private SensorManager mSensorManager2;
     private Sensor mAmbientLight;
-
-    static float x;
-    static float y;
-    static float z;
+    private SensorManager mSensorManager3;
+    private Sensor mPressure;
+    private SensorManager mSensorManager4;
+    private Sensor mTemp;
+    private TelephonyManager telephonyManager;
+    private ConnectivityManager connManager;
+    private WifiManager wifiman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +47,21 @@ public class Test extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_test);
         tv1 = (EditText)findViewById(R.id.statusText);
 
+        telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        wifiman = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager2 = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAmbientLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager3 = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager4 = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mTemp = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
 
 
 
@@ -54,12 +73,33 @@ public class Test extends Activity implements SensorEventListener {
                     startActivity(intent);
                     //finish();*/
                     LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                    tv1.setText("The Time is:  \n" + SysStrings.getTheTime() + "\n-----------------\n"  + "Your location is: \n" + SysStrings.getTheGPS(locationManager)
-                    + "\n-----------------\n"  + "The Orientation is: " + SysStrings.getOrientation(getApplicationContext()) + "\n-----------------\n"  +
-                    "The gravity is: \n" + SysStrings.getGravity() + "\n-----------------\n" + "The light is: \n" + SysStrings.getLight() + "\n-----------------\n"
-                    );                }
+                    tv1.setText("The Time is:  \n" + SysStrings.getTheTime() + "\n-----------------\n" + "Your location is: \n" + SysStrings.getTheGPS(locationManager)
+                                    + "\n-----------------\n" + "The Orientation is: " + SysStrings.getOrientation(getApplicationContext()) + "\n-----------------\n" +
+                                    "The gravity is: \n" + SysStrings.getGravity(mSensorManager) + "\n-----------------\n" + SysStrings.getLight(mSensorManager) + "\n-----------------\n"
+                                    + SysStrings.getPressure(mSensorManager) + "\n-----------------\n" + SysStrings.getTemp(mSensorManager) + "\n-----------------\n" + "The IMEI Number is: " + SysStrings.getIEMI(telephonyManager) + "\n-----------------\n"
+                                    +  SysStrings.getWIFI(connManager,wifiman) + "\n-----------------\n"
+                    );
+                }
             });
-        }    }
+        }
+
+    final Button button2 = (Button) findViewById(R.id.button2);
+        if ( button2 != null) {
+            button2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    String list = "";
+                    List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+                    for(int i =0;i<deviceSensors.size();i++) {
+                        list +=deviceSensors.get(i).getName() + "\n\n";
+
+                    }
+                    tv1.setText(list);
+
+                }
+            });
+
+        }
+    }
 
 
 
@@ -84,6 +124,8 @@ public class Test extends Activity implements SensorEventListener {
     {
         // Do something here if sensor accuracy changes.
     }
+
+    //Check for type of sensor being used then update accordingly
     public final void onSensorChanged(SensorEvent event) {
         Sensor thisSensor = event.sensor;
         if (thisSensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -92,13 +134,23 @@ public class Test extends Activity implements SensorEventListener {
         else if (thisSensor.getType() == Sensor.TYPE_LIGHT){
             SysStrings.updateLight(event);
         }
+        else if (thisSensor.getType() == Sensor.TYPE_PRESSURE){
+            SysStrings.updatePressure(event);
+        }
+        else if (thisSensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
+            SysStrings.updateTemp(event);
+        }
     }
+
     @Override
     protected void onResume()
     {
         super.onResume();
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager2.registerListener(this, mAmbientLight, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager3.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager4.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+
 
     }
     @Override
@@ -107,6 +159,9 @@ public class Test extends Activity implements SensorEventListener {
         super.onPause();
         mSensorManager.unregisterListener(this);
         mSensorManager2.unregisterListener(this);
+        mSensorManager3.unregisterListener(this);
+        mSensorManager4.unregisterListener(this);
+
 
     }
 }
