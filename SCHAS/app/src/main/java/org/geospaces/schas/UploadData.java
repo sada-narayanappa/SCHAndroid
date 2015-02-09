e496df157d5ed19e1de5f8f377abd1c6b837b000
Michael Fleming <michael.fleming121@gmail.com>
2015-02-09 12:54:25 -0600

    Beginning implementing upload data page

--- SCHAS/app/src/main/java/org/geospaces/schas/UploadData.java
+++ SCHAS/app/src/main/java/org/geospaces/schas/UploadData.java
@@ -1,19 +1,55 @@
 package org.geospaces.schas;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.commonsware.cwac.locpoll.LocationPoller;
 
 import org.geospaces.schas.R;
 
 public class UploadData extends ActionBarActivity {
 
+    private static final int PERIOD = 2 * 1000 * 60;  // 2 min
+    private PendingIntent pi = null;
+    private AlarmManager mgr = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_data);
+
+        findViewById(R.id.serviceButton).setOnClickListener(start_service_button);
     }
+    private View.OnClickListener start_service_button = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mgr = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            Intent i = new Intent(UploadData.this, LocationPoller.class);
+
+            i.putExtra(LocationPoller.EXTRA_INTENT, new Intent(UploadData.this, GPSWakfulReciever.class));
+            i.putExtra(LocationPoller.EXTRA_PROVIDER, LocationManager.GPS_PROVIDER);
+
+            pi = PendingIntent.getBroadcast(UploadData.this, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime(),
+                    PERIOD,
+                    pi);
+            Toast.makeText(UploadData.this, "Location polling every 2 minutes begun", Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
 
 
     @Override
