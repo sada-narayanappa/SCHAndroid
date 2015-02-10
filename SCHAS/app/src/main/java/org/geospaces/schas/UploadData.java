107768b1b7931e119825a57e8ddf339666ad7cce
Michael Fleming <michael.fleming121@gmail.com>
2015-02-09 21:32:18 -0600

    Update

--- SCHAS/app/src/main/java/org/geospaces/schas/UploadData.java
+++ SCHAS/app/src/main/java/org/geospaces/schas/UploadData.java
@@ -1,15 +1,20 @@
 package org.geospaces.schas;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.commonsware.cwac.locpoll.LocationPoller;
@@ -21,6 +26,8 @@ public class UploadData extends ActionBarActivity {
     private static final int PERIOD = 2 * 1000 * 60;  // 2 min
     private PendingIntent pi = null;
     private AlarmManager mgr = null;
+    private String PEF_Text;
+    private String FEV_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +35,9 @@ public class UploadData extends ActionBarActivity {
         setContentView(R.layout.activity_upload_data);
 
         findViewById(R.id.serviceButton).setOnClickListener(start_service_button);
+        findViewById(R.id.PFMButton).setOnClickListener(pfm_button);
     }
+
     private View.OnClickListener start_service_button = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -50,7 +59,65 @@ public class UploadData extends ActionBarActivity {
 
     };
 
+    private View.OnClickListener pfm_button = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String PEF = "";
+            String FEV = "";
 
+             InputTextPopUpCreator("Record PEF/FEV");
+
+        }
+    };
+
+
+
+
+    public void InputTextPopUpCreator(String Label) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(Label);
+
+        final LinearLayout ll = new LinearLayout(this);
+        final EditText input = new EditText(this);
+        final EditText input2 = new EditText(this);
+
+        input.setHint("PEF");
+        input2.setHint("FEV");
+
+        ll.addView(input);
+        ll.addView(input2);
+
+        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        input2.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        builder.setView(ll);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                PEF_Text = input.getText().toString();
+                FEV_Text = input2.getText().toString();
+
+                writeFile(PEF_Text,FEV_Text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void writeFile(String s1, String s2)
+    {
+        PEF_Text = "";
+        FEV_Text = "";
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
